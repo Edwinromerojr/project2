2a740c482ae27ee841ed507945b34533d24cb6ed
Edwin Romero <edwinrmrjr@gmail.com>
2023-01-04 13:55:38 +0800

    error fixed

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -19,6 +19,8 @@ class Listing(models.Model):
     isActive = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    watchlist = models.ManyToManyField(User, blank=True, related_name="watchlist")
+
 
     def __str__(self):
         return  self.title
--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -81,8 +81,6 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'auctions.User'
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -120,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#inserted
+AUTH_USER_MODEL = 'auctions.User'
